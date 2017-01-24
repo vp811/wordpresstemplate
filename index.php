ee7b7b1e896cef7ff338b7b68916e529448dd50c
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-01-23 19:01:04 -0500

    Create Blank files for Template

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Efren Vasquez Blank Template</title>
+</head>
+<body>
+  <h1>Blank WordPress Template</h1>
+	<p>This is my WordPress template.</p>
+</body>
+</html>
